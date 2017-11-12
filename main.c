040cfb2e3fe50f3d5d12519062b2d151f15e96fc
Rafa de Vega <rdevega@goku>
2017-11-12 15:31:38 +0100

    Makefile added and gitignore edited

---
+++ main.c
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.c
+ * Author: rdevega
+ *
+ * Created on 12 de noviembre de 2017, 15:27
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    printf("hello world\n");
+
+    return (EXIT_SUCCESS);
+}
+
